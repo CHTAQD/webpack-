d3847481f362be871308df5d52ddaef40f516b37
CHT <1437491084@qq.com>
2019-03-14 10:25:51 +0800

    '第二次提交c增加了less'

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 let path = require("path");
 let HWP = require("html-webpack-plugin")
 let MCEP = require("mini-css-extract-plugin");
+let MCEPS = require("mini-css-extract-plugin");
 
+// let proxy =  require(' http-proxy-middleware ')
 
 module.exports={
 	mode:'development',
@@ -14,29 +16,48 @@ module.exports={
 		port:3000,
 		progress:true, 
 		contentBase:'./dist', 
-		compress:true 
+        compress:true ,
+        proxy: { //devserver 自带的
+            '/api': {
+              target: 'locahost:8087',
+              pathRewrite: {'^/api' : ''},
+              secure: false          // 设置支持https协议的代理
+            }
+        }
+            
 	},
 	plugins:[
 		new HWP({
             template:'./src/index.html',
             filename:'index.html',
             hash:true,
-            minify:{
-                removeAttributeQuotes:true, 
-                collapseWhitespace: true,  
-            }
+            // minify:{
+            //     removeAttributeQuotes:true, 
+            //     collapseWhitespace: true,  
+            // }
         }),
         new MCEP({
             filename:"index.css",
           
+        }),
+        new MCEPS({
+            filename:"index.less",
+          
         })
+
 	],
 	module:{
         rules:[
+            // {
+            //     test:/.css$/,
+            //     use:[MCEP.loader,'css-loader','postcss-loader']
+            // },
             {
-                test:/.css$/,
-                use:[MCEP.loader,'css-loader','postcss-loader']
+                test:/.less$/,
+                use:[MCEPS.loader,'css-loader','less-loader']
+               
             }
+
         ]
 	}
 
